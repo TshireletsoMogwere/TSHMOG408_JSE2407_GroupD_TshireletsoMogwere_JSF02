f968adb259cf385acc3196a9bb8da2f14ab0076e
Tshireletso <tmogwere09@gmail.com>
2024-07-25 09:52:26 +0200

    installed svelte

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+
+ import Header from "./components/Header.svelte";
+
+</script>
+
+<Header />
+
+<style>
+  :global(body) {
+    font-family: Arial, sans-serif;
+    margin: 0;
+    padding: 0;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+    <span>E-commerce store</span>
+</header>
+
+<style>
+    header {
+       background-color: rgb(241, 213, 232);
+       color: rgba(160, 2, 86, 0.734);
+       margin: 0;
+       padding: 20px;
+       width: 100%;
+       position: fixed;
+       top: 0;
+       left: 0;  
+       right: 0;   
+       z-index: 1;
+    }
+</style>
